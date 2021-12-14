89b3acdac344010ad45a387bf25effbad90c9501
fue-le-purp <91432272+fue-le-purp@users.noreply.github.com>
2021-12-14 23:39:33 +0300

    Delete Занятие 3 directory

--- Занятие 3/1.py
+++
@@ -1,5 +0,0 @@
-# -- coding: utf-8 --
-a = int(input())
-b = int(input())
-for i in range (a, b+1):
-    print (i)
--- Занятие 3/8.py
+++
@@ -1,8 +0,0 @@
-8# -- coding: utf-8 --
-n = int(input())
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(j, sep='', end='')
-    print()
-
-    
