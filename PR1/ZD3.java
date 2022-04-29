4ebd3a8ee5c8e26ddf83235c5f1fe65bf56a4c79
fue-le-purp <91432272+fue-le-purp@users.noreply.github.com>
2022-04-29 12:30:16 +0300

    Add files via upload

---
+++ PR1/ZD3.java
@@ -0,0 +1,10 @@
+public class ZD3 {
+    public static void main(String p[]){
+    	if(p.length==2){
+    		int f = Integer.parseInt(p[0]);
+    		int s = Integer.parseInt(p[1]);
+    		System.out.println(p[0]+" + "+p[1]+" = "+(f+s));}
+    	else{
+    		System.out.println("Неверное количество параметров ");}
+    	}
+    }
