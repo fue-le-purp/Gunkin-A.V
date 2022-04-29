dee5959328e7bd25cc299c05e1fc59071292c9e1
fue-le-purp <91432272+fue-le-purp@users.noreply.github.com>
2022-04-29 11:48:06 +0300

    Update ZD1.java

--- PR1/ZD1.java
+++ PR1/ZD1.java
@@ -1,3 +1,5 @@
+//Реализовать программу,получающую на вход в качестве аргумента имя человека и выводящую ”Hello ” + имя, в противном случае, если параметр не передавался, ”Hello world”.
+
 public class ZD1 {
     public static void name(String[] args) {
         for (var ZD1: args)  {
