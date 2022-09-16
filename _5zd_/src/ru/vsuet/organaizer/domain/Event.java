cbee936984abc3ee6dda3d0b546d0fe23f168af6
fue-le-purp <91432272+fue-le-purp@users.noreply.github.com>
2022-09-16 09:55:18 +0300

    Add files via upload

---
+++ _5zd_/src/ru/vsuet/organaizer/domain/Event.java
@@ -0,0 +1,39 @@
+package ru.vsuet.organaizer.domain;
+
+import java.time.LocalDateTime;
+
+public class Event {
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Event() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
+}
+
+
+
